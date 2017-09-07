efb652edefaf8b1cc43b515b6e01f090c89301c4
asaaswvu <asaas@mix.wvu.edu>
2017-09-07 16:37:39 -0400

    Initial Commit

---
+++ NumberPrinter.java
@@ -0,0 +1,31 @@
+import java.util.LinkedList;
+import java.util.Random;
+import java.util.Collections;
+
+class NumberPrinter{
+
+    LinkedList<Integer> ll;
+
+    NumberPrinter(){
+        Random rand = new Random();
+        ll = new LinkedList<Integer>();
+        
+        for(int i=0; i<10; i++){
+            ll.add(rand.nextInt(100) + 1);
+        }
+
+        doStuff();
+    }
+
+    private void doStuff(){
+
+    }
+
+
+
+
+    public static void main(String [] args){
+        new NumberPrinter();	
+    }
+
+}
